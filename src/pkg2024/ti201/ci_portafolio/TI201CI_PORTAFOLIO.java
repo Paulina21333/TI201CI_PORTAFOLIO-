16cbb2717b18d62e9b5cc0e2a958a21f8a278c1d
pauvalencia <pauvalencia762@gmail>
2024-05-24 13:32:22 -0600

    inicio de proyecto

---
+++ src/pkg2024/ti201/ci_portafolio/TI201CI_PORTAFOLIO.java
@@ -0,0 +1,17 @@
+//ITESS-TICS
+//ENERO-JUNIO
+//CALCULO INTEGRAL PORTAFOLIO 
+//TI-201
+//24/05/2024
+//by FMP
+package pkg2024.ti201.ci_portafolio;
+
+
+public class TI201CI_PORTAFOLIO {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
